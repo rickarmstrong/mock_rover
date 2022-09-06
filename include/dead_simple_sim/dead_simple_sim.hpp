454cc1bc50b9bca4d9d51d42b598eda23675ed6d
Rick Armstrong <waitingfortheelectrician@gmail.com>
2022-09-05 19:49:20 -0700

    Working gtests.

--- include/dead_simple_sim/dead_simple_sim.hpp
+++ include/dead_simple_sim/dead_simple_sim.hpp
@@ -1,6 +1,9 @@
 #ifndef DEAD_SIMPLE_SIM_HPP
 #define DEAD_SIMPLE_SIM_HPP
 
+// Shortest time delta; model is updated at 1 / MIN_TIME_STEP_S.
+const double MIN_TIME_STEP_S = 1.0;
+
 class VehicleState {
 public:
     double x{};
@@ -13,19 +16,25 @@ public:
 
 class UnicycleModel {
 public:
-    UnicycleModel(): vs(VehicleState()) {};
-    void update_model(){}
+    UnicycleModel() = default;
+    UnicycleModel(const UnicycleModel& other) = delete;
     void update_cmd_vel(const geometry_msgs::Twist& cmd_vel)
     {
-        this->cur_cmd_vel = geometry_msgs::Twist(cmd_vel);
+        std::lock_guard<std::mutex> lock(this->cur_cmd_vel_mutex);
+        cur_cmd_vel = geometry_msgs::Twist(cmd_vel);
+    }
+
+    void update_model()
+    {
+        std::lock_guard<std::mutex> lock(this->vs_mutex);
     }
 
 private:
     VehicleState vs;
-    geometry_msgs::Twist cur_cmd_vel;
-};
+    std::mutex vs_mutex;
 
-// Shortest time delta; model is updated at this rate.
-const double MIN_TIME_STEP_S = 1.0;
+    geometry_msgs::Twist cur_cmd_vel;
+    std::mutex cur_cmd_vel_mutex;
+};
 
 #endif // DEAD_SIMPLE_SIM_HPP
--- src/dead_simple_sim_node.cpp
+++ src/dead_simple_sim_node.cpp
@@ -1,20 +1,18 @@
 #include <mutex>
-#include <string>
-#include <vector>
 
 #include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
 
 #include <dead_simple_sim/dead_simple_sim.hpp>
 
-void update_model_timer_cb(const ros::TimerEvent& event, UnicycleModel& um)
+void update_model_timer_cb(const ros::TimerEvent& event, UnicycleModel* um)
 {
-    um.update_model();
+    um->update_model();
 }
 
-void cmd_vel_cb(const geometry_msgs::TwistConstPtr& cmd_vel, UnicycleModel& um)
+void cmd_vel_cb(const geometry_msgs::TwistConstPtr& cmd_vel, UnicycleModel* um)
 {
-    um.update_cmd_vel(*cmd_vel);
+    um->update_cmd_vel(*cmd_vel);
 }
 
 int main(int argc, char** argv){
@@ -23,12 +21,12 @@ int main(int argc, char** argv){
 
     UnicycleModel um;
 
-    // Model update.
+    // Model update, at (1/MIN_TIME_STEP_S) Hz.
     ros::Timer model_update_timer = nh.createTimer(
-            ros::Duration(MIN_TIME_STEP_S), boost::bind(update_model_timer_cb, _1, um));
+            ros::Duration(MIN_TIME_STEP_S), boost::bind(update_model_timer_cb, _1, &um));
 
     // cmd_vel message subscriber.
-    ros::Subscriber cmd_vel_sub = nh.subscribe<geometry_msgs::Twist>("/cmd_vel", 1, boost::bind(cmd_vel_cb,_1, um));
+    ros::Subscriber cmd_vel_sub = nh.subscribe<geometry_msgs::Twist>("/cmd_vel", 1, boost::bind(cmd_vel_cb,_1, &um));
 
     ros::spin();
     return(0);
