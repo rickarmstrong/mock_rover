6f566df41490b0640f6c9b633a278a2416dd6afc
Rick Armstrong <waitingfortheelectrician@gmail.com>
2022-09-11 16:54:19 -0700

    Simple, single-axis movment works.

--- include/mock_rover/mock_rover.hpp
+++ include/mock_rover/mock_rover.hpp
@@ -1,7 +1,9 @@
-#ifndef DEAD_SIMPLE_SIM_HPP
-#define DEAD_SIMPLE_SIM_HPP
+#ifndef MOCK_ROVER_HPP
+#define MOCK_ROVER_HPP
 #include <mutex>
 
+#include <boost/math/special_functions/sign.hpp>
+#include <Eigen/Dense>
 #include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
 
@@ -10,6 +12,7 @@ const double MIN_TIME_STEP_S = 0.1;
 
 // Vehicle dymamics.
 const double MAX_LINEAR_ACC = 0.5; // m/s/s
+const double MAX_DELTA_V = MAX_LINEAR_ACC *  MIN_TIME_STEP_S;
 const double MAX_ANGULAR_VEL = 1.0; // radians/s
 
 class VehicleState {
@@ -35,7 +38,21 @@ public:
     void update_model()
     {
         std::scoped_lock<std::mutex, std::mutex> lock(vs_mutex, cur_cmd_vel_mutex);
-        vs.x_dot += (MAX_LINEAR_ACC * MIN_TIME_STEP_S);
+
+        // Update velocities (x, theta). Dont' bother with y; Unicycle model does
+        // not allow movement in the vehicle y-axes.
+        double v_diff = cur_cmd_vel.linear.x - vs.x_dot;
+        if(fabs(v_diff) >= MAX_DELTA_V)
+        {
+            vs.x_dot += MAX_DELTA_V * boost::math::sign(v_diff);
+        }
+        else
+        {
+            vs.x_dot = cur_cmd_vel.linear.x;
+        }
+
+        // Update pose.
+        vs.x += vs.x_dot * MIN_TIME_STEP_S;
     }
 
     VehicleState get_vehicle_state()
@@ -60,4 +77,4 @@ void cmd_vel_cb(const geometry_msgs::TwistConstPtr& cmd_vel, UnicycleModel* um)
 {
     um->update_cmd_vel(*cmd_vel);
 }
-#endif // DEAD_SIMPLE_SIM_HPP
+#endif // MOCK_ROVER_HPP
--- test/test_mock_rover.cpp
+++ test/test_mock_rover.cpp
@@ -17,20 +17,34 @@ TEST(UnicycleModelTest, ZeroToOne) {
     ASSERT_TRUE(um.get_vehicle_state().x == 0.0);
 
     ////////
-    // Set a non-zero velocity, and step the model forward.
+    // Set a non-zero linear velocity, and step the model forward.
     geometry_msgs::Twist cmd_vel;
     cmd_vel.linear.x = 1.0; // 1 m/s forward
 
     // Should take this long to reach 1.0 m/s, if t = v_final / a_max
-    double t = cmd_vel.linear.x / MAX_LINEAR_ACC;
-    int n_steps = (t / MIN_TIME_STEP_S); // ...which is this many steps.
+    double t_final = cmd_vel.linear.x / MAX_LINEAR_ACC;
+    int n_steps = (t_final / MIN_TIME_STEP_S); // ...which is this many steps.
     um.update_cmd_vel(cmd_vel);
-    for (int i = 0; i < n_steps; ++i)
+    for(int i = 0; i < n_steps; ++i)
     {
         um.update_model();
+        std::cout << "x_dot: "<< um.get_vehicle_state().x_dot << " "
+                  << "x: " << um.get_vehicle_state().x << "\n";
     }
     double vx = um.get_vehicle_state().x_dot;
     ASSERT_TRUE(vx == 1.0);
+
+    // Now, set a command velocity of zero and stop the rover.
+    cmd_vel.linear.x = 0.0; // stop
+    um.update_cmd_vel(cmd_vel);
+    for(int i = 0; i < n_steps; ++i)
+    {
+        um.update_model();
+        std::cout << "x_dot: "<< um.get_vehicle_state().x_dot << " "
+                  << "x: " << um.get_vehicle_state().x << "\n";
+    }
+    vx = um.get_vehicle_state().x_dot;
+    ASSERT_TRUE(vx == 0.0);
 }
 
 int main(int argc, char **argv){
