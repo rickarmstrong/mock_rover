45ecf53cd92bfcc1705c2fc614aea72d2dd7a95a
Rick Armstrong <waitingfortheelectrician@gmail.com>
2022-09-25 22:15:04 -0700

    New file gps.hpp. Removed circular include. Deleted default constructor for class Sensor.

---
+++ include/mock_rover/gps.hpp
@@ -0,0 +1,8 @@
+//
+// Created by rick on 9/25/22.
+//
+
+#ifndef MOCK_ROVER_GPS_HPP
+#define MOCK_ROVER_GPS_HPP
+
+#endif //MOCK_ROVER_GPS_HPP
--- include/mock_rover/sensor.hpp
+++ include/mock_rover/sensor.hpp
@@ -4,16 +4,23 @@
 #include <utility>
 
 #include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
-#include "mock_rover/mock_rover.hpp"
 #include "mock_rover/unicycle_model.hpp"
 
+// Base class for a sensor model that takes the instantaneous vehicle state,
+// transforms the data in some way, and publishes on a single ROS topic.
+// We assume that every subclass publishes to _one_ topic.
 class Sensor {
 public:
-    Sensor() = default;
+    Sensor() = delete;
+    explicit Sensor(const std::unique_ptr<ros::Publisher>& pub) : pub_(pub) {}
     virtual ~Sensor() = 0;
     virtual void update(const ros::TimerEvent& event, const VehicleState& vs) = 0;
+
+protected:
+        const std::unique_ptr<ros::Publisher>& pub_;
 };
 Sensor::~Sensor() = default;
 
